840458c58a057d27ffc49f3a8b863afbd97d7f92
Rhoswen <rhoswen1808@gmail.com>
2026-09-04 17:17:35 +0530

    Learnt about list comprehension, that allows me to make a list in a concise way.

---
+++ Practice problems/list_comprehension.py
@@ -0,0 +1,6 @@
+doubles = [i * 2 for i in range(1,11)]
+triples = [i * 3 for i in range(1,11) if i % 3 == 0]
+squares = [i ** 2 for i in range(1,11) ]
+print(doubles)
+print(triples)
+print(squares)
