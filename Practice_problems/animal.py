be6c5dd26ca97c2f054c6f12e0e8c2719e1dbb90
Rhoswen <rhoswen1808@gmail.com>
2026-09-13 23:49:28 +0530

    Created a class called animal having further subclasses like dog and cat.

---
+++ Practice_problems/animal.py
@@ -0,0 +1,21 @@
+class Animal:
+    def __init__(self,name):
+        self.name = name
+        self.is_alive = True
+
+    def eat(self):
+        print(f"{self.name} is eating.")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping.")
+
+class Dog(Animal):
+    def bark(self):
+        print(f"{self.name} is barking!")
+
+class Cat(Animal):
+    def meow(self):
+        print(f"{self.name} is meowing!")
+
+    def pur(self):
+        print(f"{self.name} is purring.")
